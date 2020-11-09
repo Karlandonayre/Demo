2c45518ee2ea309bf2b6e25a4e6d17e9076eaf4b
Karlandonayre <karlandonayre@gmail.com>
2020-11-08 22:28:11 -0500

    Commit prueba6 parametros

--- url.js
+++ url.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	let urlparams = window.location.search
-	console.log(urlparams);
 	_globalids = urlparams.substring(1);
 	(_globalids==undefined || _globalids==' ') ? _globalids = "undefined": "";
 	cargar(_globalids);
