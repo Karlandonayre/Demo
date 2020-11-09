0527a9228309c2985aa284f002f2b17912d60958
Karlandonayre <karlandonayre@gmail.com>
2020-11-08 22:15:32 -0500

    Commit prueba5 parametros

--- url.js
+++ url.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	let urlparams = window.location.search
+	console.log(urlparams);
 	_globalids = urlparams.substring(1);
 	(_globalids==undefined || _globalids==' ') ? _globalids = "undefined": "";
 	cargar(_globalids);
 
 	function cargar(_globalids){
-		parametros = _globalids.split('%20')
 		console.log(parametros);
 	}
 });
