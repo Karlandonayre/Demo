bae582cf313cef06373692de66d9c0be6e20c079
Karlandonayre <karlandonayre@gmail.com>
2020-11-10 16:48:46 -0500

    commit prueba split parametros

--- url.js
+++ url.js
@@ -1,12 +1,56 @@
+require(
+  [
+    "esri/identity/IdentityManager",
+    "esri/core/urlUtils",
+    "esri/layers/FeatureLayer",
+    "esri/tasks/QueryTask",
+    "esri/tasks/support/Query",
+    "esri/core/watchUtils",
+    "dojo/_base/array",  
+          
+    "dojo/domReady!"
+  ],
+  function(
+    IdentityManager,
+    urlUtils, 
+    FeatureLayer,
+    QueryTask,
+    Query,
+    watchUtils,
+    array
+  ){
+  	//servicio
+  	var url_prueba = "https://services6.arcgis.com/qHdoJI2FoNfsEzow/arcgis/rest/services/tabla_servicio_dash/FeatureServer";
+  	//Fields
+  	var fobjectid = "OBJECTID";
+  	var fregisthidroc = "REGISTRO_DE_HIDORCARBUROS";
+  	var frsocial = "RAZON_SOCIAL";
+  	var fnombdepart = "DEPARTAMENTO";
+  	var fnombprov = "PROVINCIA";
+  	var fnombdist = "DISTRITO";
+  	var fdireccion = "DIRECCION";
+  	var factividad = "ACTIVIDAD";
+  	var coddepart = "CODDEPARTAMENTO";
+  	var codprov = "CODDPROVINCIA";
+  	var coddist = "UBIGEO";
 
-$("#btn_export").on('click', function(){
-	let urlparams = window.location.search
-	_globalids = urlparams.substring(1);
-	(_globalids==undefined || _globalids==' ') ? _globalids = "undefined": "";
-	cargar(_globalids);
+  	$("#btn_export").on('click', function(){
+		let urlparams = window.location.search
+		_params = urlparams.substring(1);
+		(_params==undefined || _params==' ') ? _params = "undefined": "";
+		cargar(_params);
 
-	function cargar(_globalids){
-		console.log(_globalids);
-	}
-	
-});
+		function cargar(_params){
+			console.log(_params);
+			var parametros = (_params.split('&')).split('=')[1];
+			console.log(parametros);
+		}		
+	});
+
+
+  })
+
+
+
+
+
