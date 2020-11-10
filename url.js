d37baf71817fd401bd8c5141bdf404694d7ca89f
Karlandonayre <karlandonayre@gmail.com>
2020-11-10 18:33:36 -0500

    commit split parametros

--- url.js
+++ url.js
@@ -19,48 +19,76 @@ require(
     watchUtils,
     array
   ){
-  	//servicio
-  	var url_prueba = "https://services6.arcgis.com/qHdoJI2FoNfsEzow/arcgis/rest/services/tabla_servicio_dash/FeatureServer";
-  	//Fields
-  	var fobjectid = "OBJECTID";
-  	var fregisthidroc = "REGISTRO_DE_HIDORCARBUROS";
-  	var frsocial = "RAZON_SOCIAL";
-  	var fnombdepart = "DEPARTAMENTO";
-  	var fnombprov = "PROVINCIA";
-  	var fnombdist = "DISTRITO";
-  	var fdireccion = "DIRECCION";
-  	var factividad = "ACTIVIDAD";
-  	var fcoddepart = "CODDEPARTAMENTO";
-  	var fcodprov = "CODDPROVINCIA";
-  	var fcoddist = "UBIGEO";
+    //servicio
+    var url_prueba = "https://services6.arcgis.com/qHdoJI2FoNfsEzow/ArcGIS/rest/services/tabla_servicio_dash/FeatureServer/0";
+    //Fields
+    var fobjectid = "OBJECTID_";
+    var fregisthidroc = "REGISTRO_DE_HIDORCARBUROS_";
+    var frsocial = "RAZON_SOCIAL_";
+    var fnombdepart = "DEPARTAMENTO_";
+    var fnombprov = "PROVINCIA_";
+    var fnombdist = "DISTRITO_";
+    var fdireccion = "DIRECCION_";
+    var factividad = "ACTIVIDAD_";
+    var fcoddepart = "CODDEPARTAMENTO_";
+    var fcodprov = "CODDPROVINCIA_";
+    var fcoddist = "UBIGEO_";
 
-  	$("#btn_export").on('click', function(){
-		let urlparams = window.location.search
-		_params = urlparams.substring(1);
-		(_params==undefined || _params==' ') ? _params = "undefined": "";
-		cargar(_params);
+    $("#btn_export").on('click', function(){
+      let urlparams = window.location.search
+      _params_url = urlparams.substring(1);
+      (_params_url==undefined || _params_url==' ') ? _params_url = "undefined": "";
+      cargar(_params_url);    
+    });
 
-		function cargar(_params){
-			var parametros = _params.split('&')
-			console.log(parametros);
-			arrayParam = [];
-			for (var i = 0; i < parametros.length; i++) {
-				param = parametros[i].split('=')[1];
-				arrayParam.push(param);
-			}
-			coddepart = arrayParam[1];
-			codprov = arrayParam[2];
-			coddist = arrayParam[3];
-			console.log(arrayParam);
-			console.log(coddepart, codprov, coddist);
+    function cargar(_params_url){
+      var parametros = _params_url.split('&');
+      //prueba
+      var parametros = ["codDepart=15","codProv=", "codDist="];
+      arrayParam = [];
+      for (var i = 0; i < parametros.length; i++) {
+        parametro = parametros[i].split('=')[1];
+        arrayParam.push(parametro);
+      }
+      var coddepart = arrayParam[0].toString();
+      var codprov = arrayParam[1].toString();
+      var coddist = arrayParam[2].toString();
+      
+      console.log("departamento = ", coddepart,"provincia = " ,codprov, "distrito = " , coddist); 
+      if(coddist != ""){
+        porDistrito(coddist);
+      }else if(codprov != ""){
+        porProvincia(codprov);
+      }else if(coddepart != ""){
+        porDepartamento(coddepart);
+      }
+    } 
 
-		}		
-	});
-
+    function porDepartamento(coddepart){
+        var sql = "CODDEPARTAMENTO_ = "+coddepart;
+        console.log(sql);
+        var query = new QueryTask({url:url_prueba});
+        var params = new Query;
+        params.returnGeometry = false;
+        params.outFields = ["*"];
+        params.where = sql;
+        query.execute(params).then(function(response){
+          console.log(response);
+          if(response.features.length === 0){
+            console.log("sin registros");
+          }else{
+            var registros = response.features;
+            for (var i = 0; i < registros.length; i++) {
+              var atributos = registros[i].attributes;
+              var departamento = atributos[fnombdepart];
+              var provincia = atributos[fnombprov];
+              var distrito = atributos[fnombdist];
+              var rsocial = atributos[frsocial];
+              var actividad = atributos[factividad];
+              console.log(departamento);
+            }
+          }
+        });
+    }
 
   })
-
-
-
-
-
